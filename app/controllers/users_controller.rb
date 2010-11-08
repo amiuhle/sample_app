64e26db2a382c91cafd6135e3e4d8eb084da3234
Timo Uhlmann <timouhlmann@googlemail.com>
2010-11-08 21:05:51 +0100

    Added user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
     @title = @user.name
   end
   
@@ -52,10 +53,6 @@ class UsersController < ApplicationController
   
   private
 
-    def authenticate
-      deny_access unless signed_in?
-    end
-    
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -87,7 +87,15 @@ describe UsersController do
       response.should have_selector("h1>img", :class => "gravatar")
     end
     
-  end
+    it "should show the user's microposts" do
+      mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
+      mp2 = Factory(:micropost, :user => @user, :content => "Baz quux")
+      get :show, :id => @user
+      response.should have_selector("span.content", :content => mp1.content)
+      response.should have_selector("span.content", :content => mp2.content)
+    end
+    
+  end #GET show
 
   describe "GET 'new'" do
     it "should be successful" do
